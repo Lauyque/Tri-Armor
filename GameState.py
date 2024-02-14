3ce439bb1492011cd71f16952631a9c521089b7b
Lauyque <loic.ledoher@gmail.com>
2024-02-14 12:03:59 +0100

    suppression

--- GameState.py
+++
@@ -1,2 +0,0 @@
-class GameState:
-    maps_deverrouillees = {1: True, 2: False, 3: False, 4: False}
--- mobs.py
+++
@@ -1,7 +0,0 @@
-class Mobs():
-
-    def __init__(self, vie, degats):
-        self.vie = vie
-        self.degats = degats
-
-    
